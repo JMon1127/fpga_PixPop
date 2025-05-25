2f489129d96bef4a1ad5fee2f0722aa3a77cea81
J. Montes <jmonte11@jh.edu>
2025-05-25 00:51:55 -0500

    this timing looks better, just need to exit href state

--- simulation/tb_src/ov7670_cam_model.vhd
+++ simulation/tb_src/ov7670_cam_model.vhd
@@ -38,7 +38,64 @@ entity ov7670_cam_model is
 end ov7670_cam_model;
 
 architecture rtl of ov7670_cam_model is
+  type sm_cam_timing is ( tReset,
+                          tVsync_pulse,
+                          tVsync_blank,
+                          tHref,
+                          tFrame_end
+                        );
+
+  signal s_drive_cam : sm_cam_timing;
+
+  signal s_vsync : std_logic;
+  signal s_href  : std_logic;
+  signal s_vsync_hi_cntr : integer := 0;
+  signal s_vsync_lo_cntr : integer := 0;
+  signal s_href_cntr     : integer := 0;
 
 begin
+
+  proc_cam_model : process (I_CAM_RST_N, I_CAM_XCLK)
+  begin
+    if(I_CAM_RST_N ='0') then
+      s_drive_cam <= tReset;
+    elsif(rising_edge(I_CAM_XCLK)) then
+      case s_drive_cam is
+        when tReset       =>
+          s_vsync     <= '0';
+          s_href      <= '0';
+          s_drive_cam <= tVsync_pulse;
+        when tVsync_pulse =>
+            s_vsync <= '1';
+            if(s_vsync_hi_cntr = 4704 -1) then
+              s_vsync_hi_cntr <= 0;
+              s_drive_cam     <= tVsync_blank;
+            else
+              s_vsync_hi_cntr <= s_vsync_hi_cntr + 1;
+            end if;
+        when tVsync_blank =>
+          s_vsync <= '0';
+          if(s_vsync_lo_cntr = 26656 -1) then
+            s_vsync_lo_cntr <= 0;
+            s_drive_cam     <= tHref;
+          else
+            s_vsync_lo_cntr <= s_vsync_lo_cntr + 1;
+          end if;
+        when tHref        =>
+          if(s_href_cntr = 1568 - 1) then
+            s_href_cntr <= 0;
+          else
+            s_href_cntr <= s_href_cntr + 1;
+          end if;
+
+          if(s_href_cntr <= 1280 - 1) then
+            s_href <= '1';
+          else
+            s_href <= '0';
+          end if;
+        when tFrame_end   =>
+      end case;
+    end if;
+  end process;
 
 end architecture rtl;
