b065a416f48b5876f0f5f9a08dae549436e23c77
J. Montes <jmonte11@jh.edu>
2025-05-31 21:16:23 -0500

    added tcl script to create projet

---
+++ build/tcl/mk_proj.tcl
@@ -0,0 +1,31 @@
+# This creates the libero project
+new_project -location {..\PixPop_fpga} \
+            -name {PixPop_fpga} \
+            -project_description {} \
+            -block_mode 0 \
+            -standalone_peripheral_initialization 0 \
+            -instantiate_in_smartdesign 1 \
+            -ondemand_build_dh 1 \
+            -use_relative_path 0 \
+            -linked_files_root_dir_env {} \
+            -hdl {VHDL} \
+            -family {IGLOO2} \
+            -die {M2GL010T} \
+            -package {484 FBGA} \
+            -speed {STD} \
+            -die_voltage {1.2} \
+            -part_range {COM} \
+            -adv_options {DSW_VCCA_VOLTAGE_RAMP_RATE:100_MS} \
+            -adv_options {IO_DEFT_STD:LVCMOS 2.5V} \
+            -adv_options {PLL_SUPPLY:PLL_SUPPLY_25} \
+            -adv_options {RESTRICTPROBEPINS:1} \
+            -adv_options {RESTRICTSPIPINS:0} \
+            -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} \
+            -adv_options {TEMPR:COM} \
+            -adv_options {VCCI_1.2_VOLTR:COM} \
+            -adv_options {VCCI_1.5_VOLTR:COM} \
+            -adv_options {VCCI_1.8_VOLTR:COM} \
+            -adv_options {VCCI_2.5_VOLTR:COM} \
+            -adv_options {VCCI_3.3_VOLTR:COM} \
+            -adv_options {VOLTR:COM} \
+
